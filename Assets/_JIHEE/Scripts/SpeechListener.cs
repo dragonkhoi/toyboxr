b5e75e31264078b29fda68d8cc14f364b9c789ba
Jihee Hwang <alarmringing@gmail.com>
2019-04-27 15:08:39 -0700

    Scripts for speech recognition

---
+++ Assets/_JIHEE/Scripts/SpeechListener.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Windows.Speech;
+
+public class SpeechListener : MonoBehaviour
+{
+
+
+    //[SerializeField]
+    //private Text m_Hypotheses;
+
+
+    //[SerializeField]
+    //private Text m_Recognitions;
+
+    private DictationRecognizer m_DictationRecognizer;
+
+    List<string> recognitions = new List<string>();
+
+
+    void Start()
+    {
+        m_DictationRecognizer = new DictationRecognizer();
+
+        m_DictationRecognizer.DictationResult += (text, confidence) =>
+        {
+            Debug.LogFormat("Dictation result: {0}", text);
+            //m_Recognitions.text += text + "\n";
+            recognitions.Add(text);
+        };
+
+        m_DictationRecognizer.DictationHypothesis += (text) =>
+        {
+            Debug.LogFormat("Dictation hypothesis: {0}", text);
+            //m_Hypotheses.text += text + Env;
+        };
+
+        m_DictationRecognizer.DictationComplete += (completionCause) =>
+        {
+            if (completionCause != DictationCompletionCause.Complete)
+                Debug.LogErrorFormat("Dictation completed unsuccessfully: {0}.", completionCause);
+        };
+
+        m_DictationRecognizer.DictationError += (error, hresult) =>
+        {
+            Debug.LogErrorFormat("Dictation error: {0}; HResult = {1}.", error, hresult);
+        };
+        Debug.Log("Before start: " + m_DictationRecognizer.Status.ToString());
+
+        m_DictationRecognizer.Start();
+
+        Debug.Log("After start: " + m_DictationRecognizer.Status.ToString());
+    }
+}
