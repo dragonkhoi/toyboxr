9c156bb11c8d7f55a094f3bcb17419d17b9a09ad
Kyle Qian <kylecqian@gmail.com>
2019-04-27 19:02:04 -0700

    ObjectSpawner

--- Assets/_JIHEE/Scripts/SpeechListener.cs
+++ Assets/_JIHEE/Scripts/SpeechListener.cs
@@ -1,26 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.UI;
+
 using UnityEngine.Windows.Speech;
 
 public class SpeechListener : MonoBehaviour
 {
-    // Reference script that spawns the actual prefab
-    //public ToySpawner toySpawner;
+    public static SpeechListener Instance;
 
+    public UnityEvent<List<string>> keywordsDetected;
     public TextAsset keywordsFile;
-    List<string> keywords;
+    public HashSet<string> keywords;
 
     private DictationRecognizer m_DictationRecognizer;
 
     List<string> recognitions = new List<string>();
     List<string> extractions = new List<string>();
 
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            keywords = new HashSet<string>(keywordsFile.text.Split('\n'));
+        }
+    }
+
     void Start()
     {
-        keywords = new List<string>(keywordsFile.text.Split('\n'));
-
         m_DictationRecognizer = new DictationRecognizer();
 
         m_DictationRecognizer.DictationResult += (text, confidence) =>
@@ -51,19 +60,20 @@ public class SpeechListener : MonoBehaviour
     }
 
     void SearchFromKeywords(string newString) {
+        List<string> foundWords = new List<string>();
         string[] bagOfWords = newString.Split(' ');
-        foreach(string s in bagOfWords) {
+        foreach (string s in bagOfWords) {
             foreach (string keyword in keywords)
             {
                 if (s.Trim().Equals(keyword.Trim()))
                 {
                     Debug.Log("Found match: " + keyword.Trim() + "\n");
                     extractions.Add(s);
-                    // Invoke function on that class that triggers spawning
-                    // toySpawner.SpawnNewObject(keyword);
+                    foundWords.Add(keyword.Trim());
                 }
             }
         }
+        keywordsDetected.Invoke(foundWords);
     }
 
     /*
---
+++ Assets/_KYLE/_SCRIPTS/ObjectSpawner.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectSpawner : MonoBehaviour
+{
+    [SerializeField] Vector3 defaultSpawnLocation;
+    [SerializeField] float spawnSpacing;
+
+    void Start()
+    {
+        SpeechListener.Instance.keywordsDetected.AddListener(OnKeywordDetected);
+    }
+
+    void OnKeywordDetected(List<string> foundWords)
+    {
+        for (int i = 0; i < foundWords.Count; ++i)
+        {
+            GameObject prefab = (GameObject)Resources.Load(foundWords[i], typeof(GameObject));
+            Vector3 spawnLocation = defaultSpawnLocation;
+            spawnLocation.y += spawnSpacing * i;
+            Instantiate(prefab, spawnLocation, Quaternion.identity);
+        }
+    }
+}
